4c90c85888d8f40c781bea2a23d6f6e241589fa5
Anna <kochurovaav@gmail.com>
2023-02-06 14:58:20 +0200

    Task01 is finished

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -3,5 +3,37 @@
 
 System.Console.WriteLine("Enter five-digit number:     ");
 
-string strNumberOne = Console.ReadLine();
-int NumberA = Convert.ToInt32(strNumberOne);
+string strNumberA = Console.ReadLine();
+int NumberA = Convert.ToInt32(strNumberA);
+
+int NumberB = NumberA/10000;
+int NumberC = NumberA%10;
+int NumberD = (NumberA%10000)/10; 
+
+if (NumberB == NumberC) 
+{
+     NumberD = (NumberA%10000)/10; 
+}
+
+    int result = ((NumberA%10000) - (NumberA%10)) / 10;
+    int result2 = result/100;
+    int result3 = result%10;
+
+if (result2 == result3)
+{
+    System.Console.WriteLine("Its a palindrom");
+}
+else {
+    System.Console.WriteLine("Its not a palindrom");
+}
+
+
+
+
+
+
+
+
+
+ 
+
